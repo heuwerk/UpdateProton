ae9155bea270550cd1cab31dc88f1c08418caff6
heuwerk <heuwerk@mailbox.org>
2021-10-13 22:14:33 +0200

    code cleanup

--- updateproton.sh
+++ updateproton.sh
@@ -5,20 +5,20 @@
 # definition of constant variables
 readonly WEBSITE='https://github.com/GloriousEggroll/proton-ge-custom/releases/latest'
 readonly REGEX='<a.*tag\/.*GE-[0-9]*"'
-readonly PROTON_PATH="$HOME/.steam/root/compatibilitytools.d"
+readonly PROTON_PATH="${HOME}/.steam/root/compatibilitytools.d"
 
 # checks if all required directories are present
 prerequirements() {
 	# check if .steam dir is present
-	[ ! -d "$HOME/.steam" ] && printf "ERROR: Steam not installed!\n" && exit 1
+	[ ! -d "${HOME}/.steam" ] && printf "ERROR: Steam not installed!\n" && exit 1
 
 	# check if compatibilitytools dir is present
-	[ -d "$PROTON_PATH" ] || mkdir "$PROTON_PATH"
+	[ -d "${PROTON_PATH}" ] || mkdir "${PROTON_PATH}"
 }
 
 get_new_version() {
 	# downloads the website, terminates the progam, if an error occures
-	proton_version="$(! wget "$WEBSITE" -qO- | grep -m1 -o "$REGEX")" && 
+	proton_version="$(! wget "${WEBSITE}" -qO- | grep -m1 -o "${REGEX}")" && 
     printf "ERROR: No internet connection!\n" && exit 1
 
 	# extracts the newest Proton release
@@ -26,43 +26,45 @@ get_new_version() {
   proton_version="${proton_version%\"*}"
 
 	# output of newest version
-	printf "Newest version: %s\n" "$proton_version"
+	printf "Newest version: %s\n" "${proton_version}"
 }
 
 # checks, if the newest version is already installed.
 check_installed_version() {
-  proton_installed="$(find "$PROTON_PATH" -mindepth 1 -maxdepth 1 -type d | sort -V | tail -1 )"
+  proton_installed="$(find "${PROTON_PATH}" -mindepth 1 -maxdepth 1 -type d | sort -V | tail -1 )"
   proton_installed="${proton_installed##*/}"
   proton_installed="${proton_installed#*-}"
 
-	[ "$proton_version" = "$proton_installed" ] && printf "Newest version already installed\n" && exit 0
+	[ "${proton_version}" = "${proton_installed}" ] && printf "Newest version already installed\n" && exit 0
 
-	[ -n "$proton_installed" ] && \
-        printf "Installed version: %s\n" "$proton_installed" || \
+	[ -n "${proton_installed}" ] && \
+        printf "Installed version: %s\n" "${proton_installed}" || \
         printf "Proton not installed\n"
 
-  printf "Changelog: https://github.com/GloriousEggroll/proton-ge-custom/releases/tag/%s\n" "$proton_version"
+  printf "Changelog: https://github.com/GloriousEggroll/proton-ge-custom/releases/tag/%s\n" "${proton_version}"
 
 	printf "\nInstall new version? [Y/n]: " ; read -r answer
-	case "$answer" in
+	case "${answer}" in
 		[YyJj]|[Yy]es|[Jj]a|"") update="1"
 		;;
+    *) exit 0
+    ;;
 	esac
 }
 
 # download and verify the new proton version
 download_proton() {
-    cd "$HOME" || exit 1
+  cd "${HOME}" || exit 1
 
-	if [ -n "$update" ] ; then
+	if [ -n "${update}" ] ; then
 		# generates a Path that wget can Download
-		file="${WEBSITE%/*}/download/$proton_version/Proton-$proton_version.tar.gz"
-		checksum="${WEBSITE%/*}/download/$proton_version/Proton-$proton_version.sha512sum"
+		file="${WEBSITE%/*}/download/${proton_version}/Proton-${proton_version}.tar.gz"
+		checksum="${WEBSITE%/*}/download/${proton_version}/Proton-${proton_version}.sha512sum"
 		
-    ! wget "$file" --show-progress -cqP "$HOME" && \
+    ! wget "${file}" --show-progress -cqP "${HOME}" && \
       printf "ERROR: No internet connection!\n" && exit 1
 
-		wget "$checksum" -qO- | sha512sum --quiet -c  && printf "Verification OK\n"
+		wget "${checksum}" -qO- | sha512sum --quiet -c  && printf "Verification OK\n"
 	else
 		printf "Installation aborted\n"; exit 1
 	fi
@@ -72,17 +74,19 @@ download_proton() {
 unpack_proton() {
 	proton_archive="${file##*/}"
 	
-	[ -n "$proton_installed" ] && printf "Delete ALL old versions? [y/N]: " && read -r cleanup
+	[ -n "${proton_installed}" ] && printf "Delete ALL old versions? [y/N]: " && read -r cleanup
 
-	case "$cleanup" in
+	case "${cleanup}" in
 		[YyJj]|[Yy]es|[Jj]a)
       printf "Cleanup..."
 			rm -rf "${PROTON_PATH:?}"/*
+    ;;
+    *) ;;
 	esac
 
 	# extracts the archive to the destination and deletes everything afterwards
-	tar -xzf "$HOME/$proton_archive" -C "$PROTON_PATH"
-	rm "$proton_archive"
+	tar -xzf "${HOME}/${proton_archive}" -C "${PROTON_PATH}"
+	rm "${proton_archive}"
 }
 
 prerequirements && \
