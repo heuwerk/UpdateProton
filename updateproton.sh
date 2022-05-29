5c650b75de2800542a406ef053b4d3659563ae73
heuwerk <heuwerk@mailbox.org>
2022-05-29 19:32:20 +0200

    removed script mode and help to reduce code complexity

--- updateproton.sh
+++ updateproton.sh
@@ -84,48 +84,6 @@ unpack_proton() {
 	rm "${proton_archive}"
 }
 
-# script mode, installs the latest version without disturbing the user
-script_mode() {
-    prerequirements
-
-	proton_version="$(! wget "${WEBSITE}" -qO- | grep -m1 -o "${REGEX}")" && exit 1
-    proton_version="${proton_version##*/}"
-    proton_installed="$(find "${PROTON_PATH}" -mindepth 1 -maxdepth 1 -type d | sort -V | tail -1 )"
-    proton_installed="${proton_installed##*/}"
-	file="${WEBSITE%/*}/download/${proton_version}/${proton_version}.tar.gz"
-	checksum="${WEBSITE%/*}/download/${proton_version}/${proton_version}.sha512sum"
-
-	[ "${proton_version}" = "${proton_installed}" ] && exit 0
-
-    cd "${HOME}" || exit 1
-    ! wget "${file}" -cqP "${HOME}" && exit 1
-	wget "${checksum}" -qO- | sha512sum --quiet -c
-
-    proton_archive="${file##*/}"
-	tar -xzf "${HOME}/${proton_archive}" -C "${PROTON_PATH}"
-	rm "${proton_archive}"
-
-    exit 0
-}
-
-# display help
-usage() {
-    printf "Usage: ./updateproton.sh [-s] [-h]\n"
-    printf "\t-h\n\t\tDisplay this help\n\n"
-    printf "\t-s\n\t\tScript-Mode: Downloads the latest version without deleting old versions; does not output any hints\n"
-    printf "\n\tReturn codes:\n\t\t0: Execution was successful, or latest version is already installed\n"
-    printf "\t\t1: An error occurred\n"
-    exit 1
-}
-
-while getopts hs flag; do
-    case "${flag}" in
-        h) usage ;;
-        s) script_mode ;;
-        *) ;;
-    esac
-done
-
 prerequirements && \
 get_new_version && \
 check_installed_version && \
